480b10dcdbe21582c49b169aa0af8ca1197751ee
Sergi <97520112+sergism77@users.noreply.github.com>
2023-04-09 21:40:00 +0200

    Delete database.js

--- src/database.js
+++
@@ -1,9 +0,0 @@
-//this is the database connection file in the src folder
-
-import mariadb from 'mariadb';
-import config from 'config.json';
-const dbConfig = config.database;
-let pool = mariadb.createPool(dbConfig);
-export default pool;
-
-
